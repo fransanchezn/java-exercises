7b100e7ad3593c36f6354282041dcc1a2be17a9d
Fran Sanchez <fran@grabyo.com>
2020-04-15 16:45:17 +0200

    Remove unused imports and logs

--- src/main/java/hackerank/StockAnalysis.java
+++ src/main/java/hackerank/StockAnalysis.java
@@ -2,7 +2,6 @@ package hackerank;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,7 +22,6 @@ public class StockAnalysis {
                 String[] pointSplit = point.split(":");
                 stock.put(pointSplit[0], pointSplit[1]);
             }
-            System.out.println(stock);
         }
 
         return new ArrayList<>(stock.values());
