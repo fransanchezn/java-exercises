dbbf2b890ec0183374514b0038313077d95659f1
Fran Sanchez <fran.sanchez@glovoapp.com>
2024-07-04 18:52:44 +0200

    find Nearby duplicate exercise

--- src/main/java/hashmap/ContainsNearbyDuplicate.java
+++ src/main/java/hashmap/ContainsNearbyDuplicate.java
@@ -1,2 +1,27 @@
-package hashmap;public class ContainsNearbyDuplicate {
+package hashmap;
+
+import java.util.HashMap;
+
+public class ContainsNearbyDuplicate {
+
+    public static void main(final String[] args) {
+        //final var nums = new int[]{ 1,2,3,1,2,3 };
+        final var nums = new int[]{ 1,2,3,1 };
+        System.out.println(containsNearbyDuplicate(nums, 3));
+    }
+
+    // O(n)
+    public static boolean containsNearbyDuplicate(final int[] nums, final int k) {
+        final var numberPosition = new HashMap<Integer, Integer>();
+
+        for (int i = 0; i < nums.length; i++) {
+            final var closestPrevious = numberPosition.get(nums[i]);
+            if (closestPrevious != null && i - closestPrevious <= k) {
+                return true;
+            }
+            numberPosition.put(nums[i], i);
+        }
+
+        return false;
+    }
 }
