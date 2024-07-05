9025ce5cdbaab8cb6e316aa130cc97e1bd0d99a7
Fran Sanchez <fran.sanchez@glovoapp.com>
2024-07-05 15:46:18 +0200

    stacks exercise parentheses

---
+++ src/main/java/intervals/SummaryRanges.java
@@ -0,0 +1,2 @@
+package intervals;public class SummaryRanges {
+}
---
+++ src/main/java/stacks/ValidParentheses.java
@@ -0,0 +1,36 @@
+package stacks;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class ValidParentheses {
+
+    private static final Map<Character, Character> PARENTHESIS = new HashMap<>() {{
+        put(')', '(');
+        put(']', '[');
+        put('}', '{');
+    }};
+
+    public static void main(final String[] args) {
+        System.out.println(isValid("()"));
+    }
+
+    public static boolean isValid(final String s) {
+        final var stack = new Stack<Character>();
+
+        for (int i = 0; i < s.length(); i++) {
+            final var character = s.charAt(i);
+            final var closingParenthesis = PARENTHESIS.get(character);
+            if (closingParenthesis == null) {
+                stack.push(character);
+            } else if (stack.isEmpty() || stack.peek() != closingParenthesis) {
+                return false;
+            } else {
+                stack.pop();
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
