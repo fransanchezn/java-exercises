cd85a4101c6d32c7f1b31ed82bfe180cb4612a08
Fran Sanchez <fran.sanchez@glovoapp.com>
2024-09-10 18:55:28 +0200

    Backtracking first exercises

---
+++ src/main/java/exercises/backtracking/AllPathsFromSourceTarget.java
@@ -0,0 +1,28 @@
+package exercises.backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AllPathsFromSourceTarget {
+    public List<List<Integer>> allPathsSourceTarget(final int[][] graph) {
+        final var ans = new ArrayList<List<Integer>>();
+        final var curr = new ArrayList<Integer>() {{
+            add(0);
+        }};
+        backtrack(curr, 0, ans, graph);
+        return ans;
+    }
+
+    private void backtrack(final List<Integer> curr, final int node, final List<List<Integer>> ans, final int[][] graph) {
+        if (node == graph.length - 1) {
+            ans.add(new ArrayList<>(curr));
+        }
+
+        for (int i = 0; i < graph[node].length; i++) {
+            final var nextNode = graph[node][i];
+            curr.add(nextNode);
+            backtrack(curr, nextNode, ans, graph);
+            curr.removeLast();
+        }
+    }
+}
---
+++ src/main/java/exercises/backtracking/LetterCombinationsPhoneNumber.java
@@ -0,0 +1,43 @@
+package exercises.backtracking;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LetterCombinationsPhoneNumber {
+    private final Map<String, String> KEYBOARD = new HashMap<>() {{
+        put("0", "");
+        put("1", "");
+        put("2", "abc");
+        put("3", "def");
+        put("4", "ghi");
+        put("5", "jkl");
+        put("6", "mno");
+        put("7", "pqrs");
+        put("8", "tuv");
+        put("9", "wxyz");
+    }};
+
+    public List<String> letterCombinations(final String digits) {
+        final var numbers = digits.split("");
+        final var ans = new ArrayList<String>();
+        backtracking(new StringBuilder(), ans, 0, numbers);
+        return ans;
+    }
+
+    private void backtracking(final StringBuilder curr, final List<String> ans, final int index, final String[] digits) {
+        if (curr.length() == digits.length) {
+            ans.add(curr.toString());
+            return;
+        }
+
+        final var values = KEYBOARD.getOrDefault(digits[index], "");
+        for (int i = 0; i < values.length(); i++) {
+            curr.append(values.charAt(i));
+            backtracking(curr, ans, index + 1, digits);
+            curr.deleteCharAt(curr.length() - 1);
+        }
+    }
+}
